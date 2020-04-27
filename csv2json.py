01f3a167de358cd748f87ffb35a85fa857550f9a
Matthieu Berthomé <matthieu@mmea.fr>
2020-04-27 15:18:11 +0200

    Liste.csv => mouvements.json

---
+++ csv2json.py
@@ -0,0 +1,21 @@
+import csv
+import json
+import os
+
+with open('Liste.csv') as liste:
+    reader = csv.DictReader(liste)
+
+    data = []
+    for datum in reader:
+        new_datum = {}
+        for k, v in datum.items():
+            new_datum[k] = v.strip()
+
+        new_datum['Nb Postes Vacants'] = int(float(new_datum['Nb Postes Vacants'])) if new_datum['Nb Postes Vacants'] else 0
+        new_datum['Quotité'] = int(float(new_datum['Quotité'])) if new_datum['Quotité'] else 0
+        new_datum['N°POSTE'] = int(new_datum['N°POSTE'])
+        new_datum['N°POSTE'] = int(new_datum['N°POSTE'])
+        data.append(new_datum)
+
+with open(os.path.join('src', 'assets', 'mouvements.json'), 'w') as mouvements_json_file:
+    json.dump(data, mouvements_json_file, indent=2)
